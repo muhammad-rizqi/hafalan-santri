791efdcc99213ce64903b4807cde9d89c2affc4c
Muhammad Rizqi <muhammad-rizqi@live.com>
2020-12-18 09:23:00 +0700

    delete Text in import

--- src/components/GridMenu.js
+++ src/components/GridMenu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, TouchableOpacity} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import {styles} from '../styles/styles';
 
